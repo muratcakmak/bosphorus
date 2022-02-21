78c691ee4e15b20895ba810590ae8a471f0547ec
Oguzhan Cakmak <muratcakmak@users.noreply.github.com>
2022-02-21 07:21:29 -0800

    add header component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
 import { Button } from './Button';
-export { Button };
+import { Header } from './Header';
+export { Button, Header };
